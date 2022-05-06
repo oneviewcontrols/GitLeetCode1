6834b6b8fe710d290bd4a2b8e8497456d959689e
Andy SanClemente <andysanclemente@gail.com>
2022-05-06 10:25:19 -0400

    New Solution

---
+++ src/com/company/leetcode/problems/MedianOfTwoSortedArrays.java
@@ -0,0 +1,23 @@
+package com.company.leetcode.problems;
+
+import java.util.Arrays;
+
+public class MedianOfTwoSortedArrays {
+
+
+    public double findMedianSortedArraysOld(int[] nums1, int[] nums2) {
+        int[] mergedArray = new int[nums1.length + nums2.length];
+        if (mergedArray.length == 0) return 0;
+        System.arraycopy(nums1, 0, mergedArray, 0, nums1.length);
+        System.arraycopy(nums2, 0, mergedArray, nums1.length, nums2.length);
+        Arrays.sort(mergedArray);
+        if ((mergedArray.length % 2) == 0) {
+            int point1 = (mergedArray.length / 2) - 1;
+            int point2 = mergedArray.length / 2;
+            return (double) (mergedArray[point1] + mergedArray[point2]) / 2;
+        } else {
+            return mergedArray[mergedArray.length / 2];
+        }
+    }
+
+}
