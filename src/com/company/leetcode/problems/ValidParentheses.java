839a0ca6e1dcda2937b5688c18b495f07bc2c601
ansancle <andysanclemente@gmail.com>
2022-05-08 12:49:09 -0400

    New Solutions

--- src/com/company/leetcode/problems/ValidParentheses.java
+++ src/com/company/leetcode/problems/ValidParentheses.java
@@ -2,42 +2,77 @@ package com.company.leetcode.problems;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class ValidParentheses {
-    public boolean isValid(String s) {
-        if (s.length() == 1) return false;
-        if ((s.length() % 2) != 0) return false;
-        String[] str = s.split("");
-        Map<String,String> map = new HashMap<>();
-        map.put("(",")");
-        map.put("[","]");
-        map.put("{","}");
-        for (int i=0;i<s.length();i++) {
-             isValidSubString(i, s, str, map);
-        }
-        return false;
+
+    // Hash table that takes care of the map.
+    private HashMap<Character, Character> map;
+    {
+        map = new HashMap<Character, Character>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
     }
 
-    private boolean isValidSubString(int start,String s,String[] str,Map<String,String> map) {
-        if (s.length() == 0) return true;
-        String left = str[start];
-        if (map.containsKey(left) == false) return false;
-        int rightIndex = s.indexOf(map.get(left));
-        if (rightIndex == -1) return false;
-        rightIndex--;
-        if (rightIndex == start) return true;
-        String subString = s.substring(start + 1,rightIndex);
-        return isValidSubString(start,s,str,map);
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i=0;i<s.length();i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (stack.empty()) return false;
+                Character open = stack.pop();
+                if (open.charValue() != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
     }
+    
+    
+    
+    public boolean isValidold(String s) {
+
+        // Initialize a stack to be used in the algorithm.
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            // If the current character is a closing bracket.
+            if (this.map.containsKey(c)) {
+
+                // Get the top element of the stack. If the stack is empty, set a dummy value of '#'
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                // If the mapping for this bracket doesn't match the stack's top element, return false.
+                if (topElement != this.map.get(c)) {
+                    return false;
+                }
+            } else {
+                // If it was an opening bracket, push to the stack.
+                stack.push(c);
+            }
+        }
+
+        // If the stack still contains elements, then it is an invalid expression.
+        return stack.isEmpty();
+    }
+
+
 
     public static void runTestCase() {
         ValidParentheses v = new ValidParentheses();
-        String s1 = "()[]{}";
+        // [{()}][]][
+        String s1 = "[][()([]){}]";
         String s2 = "(";
         String s3 = "()]{}";
         String s4 = "()[]{";
         String s5 = ")[]{";
-        System.out.println("()[]{} = " + v.isValid(s1));
+        System.out.println("[][()([]){}] = " + v.isValid(s1));
         System.out.println("( = " + v.isValid(s2));
         System.out.println("()]{} = " + v.isValid(s3));
         System.out.println("()[]{ = " + v.isValid(s4));
