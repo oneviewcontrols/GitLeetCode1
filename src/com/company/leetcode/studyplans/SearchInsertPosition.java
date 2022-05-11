5838d23d694c807f1a7f42559ca5675cd579ef47
ansancle <andysanclemente@gmail.com>
2022-05-11 18:55:59 -0400

    New Solution

--- src/com/company/leetcode/studyplans/SearchInsertPosition.java
+++ src/com/company/leetcode/studyplans/SearchInsertPosition.java
@@ -4,17 +4,17 @@ public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        if (target > nums[right]) return nums.length;
-        int mid = right - left / 2;
-        if (nums[mid] == target) return mid;
-        if (nums[mid] > target) {
-            right = mid;
-        } else {
-            left = mid;
-        }
+        if (nums[right] < target) return nums.length;
+        if (nums[0] > target) return 0;
         while (left <= right) {
-            int v1 = nums[left];
-            int v2 = nums[right]
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target) return middle;
+            if (nums[left] == target) return left;
+            if (nums[right] == target) return right;
+            left = nums[middle] > target ? left : middle;
+            right = nums[middle] > target ? middle : right;
+            if ((left + 1) == right) return right;
         }
+        return left;
     }
 }
