385b9f00c9e765f7e7d32789d84e6b83e30f5844
ansancle <andysanclemente@gmail.com>
2022-05-13 10:47:49 -0400

    New Solution

--- src/com/company/leetcode/Main.java
+++ src/com/company/leetcode/Main.java
@@ -3,13 +3,14 @@ package com.company.leetcode;
 import com.company.leetcode.assessments.RandomCompanyFour;
 import com.company.leetcode.problems.BattleshipsInAbBoard;
 import com.company.leetcode.studyplans.MoveZeroes;
+import com.company.leetcode.studyplans.NumberOfIslands;
 import com.company.leetcode.studyplans.TwoSumTwo;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        TwoSumTwo.runTestCases();
+        NumberOfIslands.runTestCases();
     }
 }
 
