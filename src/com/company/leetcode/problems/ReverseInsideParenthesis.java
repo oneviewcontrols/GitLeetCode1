0a503838e600b1bbc14b3a3156c9e2315a6d0a08
Andy SanClemente <andysanclemente@gail.com>
2022-06-07 07:11:25 -0400

    New

---
+++ src/com/company/leetcode/problems/ReverseInsideParenthesis.java
@@ -0,0 +1,32 @@
+package com.company.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class ReverseInsideParenthesis {
+
+        public String reverseParentheses(String s) {
+            Stack<Character> list=new Stack<>();
+            for(int i=0;i<s.length();i++){
+                if(s.charAt(i)!=')'){
+                    list.push(s.charAt(i));
+                }
+                else{
+                    ArrayList<Character> li=new ArrayList<>();
+                    while(list.peek()!='('){
+                        li.add(list.pop());
+                    }
+                    list.pop();
+                    for(int j=0;j<li.size();j++){
+                        list.push(li.get(j));
+                    }
+                }
+            }
+            String ans="";
+            for(int i=0;i<list.size();i++){
+                ans+=list.get(i);
+            }
+            return ans;
+        }
+
+}
