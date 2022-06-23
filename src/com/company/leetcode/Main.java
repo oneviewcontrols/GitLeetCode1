471fcd78cf1fb288e2e18320e31f3c56274152f2
Andy SanClemente <andysanclemente@gail.com>
2022-06-23 09:58:01 -0400

    New

--- src/com/company/leetcode/Main.java
+++ src/com/company/leetcode/Main.java
@@ -8,6 +8,11 @@ import com.kayak.robot.racing.RobotRacingService;
 public class Main {
 
     public static void main(String[] args) {
+        Float f1= new Float("3.0");
+        int x = f1.intValue();
+        byte b = f1.byteValue();
+        double d = f1.doubleValue();
+        System.out.println(x + b + d);
 
     }
 
--- src/com/company/leetcode/problems/CountCharacters.java
+++ src/com/company/leetcode/problems/CountCharacters.java
@@ -9,29 +9,31 @@ public class CountCharacters {
 
     public int countCharacters(String[] words, String chars) {
 
-        int [] temp=new int[26];
-        int ans=0;
-        HashSet<Character> st=new HashSet<>();
-        for(char c:chars.toCharArray()){
-            temp[c-'a']++;
+        int[] temp = new int[26];
+        int ans = 0;
+        HashSet<Character> st = new HashSet<>();
+        for (char c : chars.toCharArray()) {
+            temp[c - 'a']++;
         }
         System.out.print(st);
-        for(String s:words){
-            if(ismatch(s,temp)){
+        for (String s : words) {
+            if (ismatch(s, temp)) {
 
-                ans+=s.length();
+                ans += s.length();
             }
         }
         return ans;
 
 
     }
-    boolean ismatch(String s,int[] temp){
-        int []temp1= Arrays.copyOf(temp, temp.length);
-        for(char str:s.toCharArray()){
-            if(temp1[str-'a']==0) return false;
-            else temp1[str-'a']--;
+
+    boolean ismatch(String s, int[] temp) {
+        int[] temp1 = Arrays.copyOf(temp, temp.length);
+        for (char str : s.toCharArray()) {
+            if (temp1[str - 'a'] == 0) return false;
+            else temp1[str - 'a']--;
 
         }
         return true;
     }
+}
