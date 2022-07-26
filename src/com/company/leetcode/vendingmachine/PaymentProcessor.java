059426c800d131374e1ce7155fc97687a453d758
Andy SanClemente <andysanclemente@gail.com>
2022-07-26 14:18:41 -0400

    Moved

--- src/com/company/leetcode/vendingmachine/PaymentProcessor.java
+++ src/com/company/leetcode/vendingmachine/PaymentProcessor.java
@@ -1,9 +1,5 @@
 package com.company.leetcode.vendingmachine;
 
-import java.util.Currency;
-import java.util.HashMap;
-import java.util.Map;
-
 public interface PaymentProcessor {
 
     public CurrencyType getType();
