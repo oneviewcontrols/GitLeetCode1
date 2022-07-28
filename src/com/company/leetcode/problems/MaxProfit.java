ecbd0514e55f74a4b465be4b072f8b7d91b6f354
Andy SanClemente <andysanclemente@gail.com>
2022-07-28 12:12:39 -0400

    New

--- src/com/company/leetcode/problems/MaxProfit.java
+++ src/com/company/leetcode/problems/MaxProfit.java
@@ -16,4 +16,16 @@ public class MaxProfit {
         return max;
 
     }
+
+    public int maxProfitTheirSolution(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
 }
