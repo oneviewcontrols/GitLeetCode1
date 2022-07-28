e21ce942ece86a9dab67faeb9176a7e377bd34ef
Andy SanClemente <andysanclemente@gail.com>
2022-07-28 06:32:02 -0400

    New

---
+++ src/com/company/leetcode/cards/StandardDeck.java
@@ -0,0 +1,33 @@
+package com.company.leetcode.cards;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StandardDeck {
+    private List<StandardCard> deck = new ArrayList<>();
+
+    public StandardDeck() {
+        super();
+        initialize();
+    }
+
+    private void initialize() {
+        // Load Map with Cards.
+    }
+
+    public void shuffle() {}
+
+    public StandardCard nextCard() {
+        return null;
+    }
+
+    public void cut() {
+
+    }
+
+
+
+
+}
--- src/com/company/leetcode/parkinglot/ParkingSpot.java
+++ src/com/company/leetcode/parkinglot/ParkingSpot.java
@@ -1,7 +1,10 @@
 package com.company.leetcode.parkinglot;
 
+import java.util.Set;
+
 public class ParkingSpot {
     private VehicleType spaceType;
+    private Set<ParkingSpotType> allows;
     private String spaceId = "";
 
 
